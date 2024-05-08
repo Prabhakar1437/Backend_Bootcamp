9e5f50616df94342a9d0cac8c6d26bc5a8dddf76
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-08 12:29:57 +0530

    Day3

--- 4routing.js
+++ 4routing.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs =require('fs');
 const url= require('url');
+const slugify = require('slugify');
 const replaceTemplate= require('./replaceTemplate');
 //const { log } = require('console');
 
@@ -16,8 +17,9 @@ const tempProduct =fs.readFileSync(`${__dirname}/template/template-product.html`
 
 const data =fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
-     
+const slugs = dataObj.map(el=>slugify(el.productName,{lower:true}));
 
+console.log(slugs);
 const server = http.createServer((req, res) => {
     
     const {query,pathname}=(url.parse(req.url,true));
